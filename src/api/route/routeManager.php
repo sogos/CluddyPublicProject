e20e9110a43ad450f2c138ba877beeec6906e80b
Thibault Cordier <thibault.cordier@gmail.com>
2012-08-28 15:31:53 +0200

    Ajout Intl + Routing

--- src/api/route/routeManager.php
+++ src/api/route/routeManager.php
@@ -1,18 +1,103 @@
 <?php
 
 namespace api\route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use api\language\languageUtils;
+
+
 
 class routeManager {
 
-	private $urls = array(
-			'goog' => 'http://www.google.com',
-			'fb' => 'http://www.facebook.com',
-			);
+	public function __construct($app){
+	
+		$fallback_language = languageUtils::getClientLanguage();
+		$app->get('/{locale}/ariane/{module}', function ($locale,$module) use ($app) {
+				return $app['twig']->render('ariane.twig', array(
+						'module' => $module,
+						'_locale' => $locale
+						));
 
-	public function get($short){
-		return $this->urls[$short];
+				})
+		->value('module', 'home')
+		->value('locale', $fallback_language)
+		->bind('_ariane');
+
+
+		$app->get('/{locale}/{module}', function ($locale,$module) use ($app) {
+				return $app['route_Manager']->get($app,$locale,$module);
+				})
+		->value('locale', $fallback_language)
+		->value('module', 'home');
+
+		$app->get('/{locale}/{module}/{action}', function ($locale,$module,$action) use ($app) {
+				return $app['route_Manager']->get($app,$locale,$module,$action);
+				})
+		->value('locale', $fallback_language)
+		->value('action','index');
+
+		$app->get('/{locale}/{module}/{action}/{id}', function ($locale,$module,$action, $id) use ($app) {
+				return $app['route_Manager']->get($app,$locale,$module,$action,$id);
+				})
+		->value('locale', $fallback_language)
+		->assert('id', '\d+');
+
+
+		$app->post('/{locale}/{module}/{action}/{id}', function ($module,$locale,$action, $id) use ($app) {
+				return $app['route_Manager']->post($request,$locale, $module,$action,$id);
+				})
+                ->value('locale', $fallback_language)
+		->assert('id', '\d+');
+		$app->get('/{locale}/home', function ($locale) use ($app) {
+				return $app['route_Manager']->get($app,"home",$locale);
+				})
+                ->value('locale', $fallback_language)
+		->bind('_homepage');
+
+
+
+/*	 $app->get('/{slug}', function ($slug) use ($app) {
+                        return $app['route_Manager']->get($app['request'],$slug);
+                        })
+        ->assert('slug', '.+');
+*/
+
+		$app->error(function (\Exception $e, $code) use ($app, $fallback_language) {
+				$app['translator']->setLocale($locale);
+				
+				return  new Response($app['twig']->render('error.twig', array(
+					'code' => $code,
+					'module' => $module,
+					'e' => $e
+					)), 200, array(
+                                                'Cache-Control' => 's-maxage=5, public',
+                                                ));
+
+			});
+
 	}
 
+
+
+	public function boot() {
+	}
+
+	public function get(\Silex\Application $app,$locale,$module = "", $action = "", $id = "") {
+				$app['translator']->setLocale($locale);
+			$app['monolog']->addWarning(sprintf(">> Calling Route: " . $module . ":" . $action . ":". $id));
+			return new Response($app['twig']->render('debug.twig', array(
+						'module' => $module,
+						'action' => $action,
+						'id' => $id,
+						)), 200, array(
+						'Cache-Control' => 's-maxage=5, public',
+						
+						));
+	}
+	
+	public function post($request ,$module = "", $action = "", $id = "" ) {
+	}
+		
 }
 
 ?>
--- web/index.php
+++ web/index.php
@@ -3,8 +3,12 @@
    web/index.php
  */
 require_once __DIR__.'/../vendor/autoload.php';
-#require_once __DIR__.'/../src/api/route/routeManager.php';
+
 use api\route\routeManager;
+use api\language\languageUtils;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Monolog\Logger;
 
 $app = new Silex\Application();
 
@@ -12,37 +16,94 @@ $env = getenv('APP_ENV') ?: 'prod';
 $app->register(new Igorw\Silex\ConfigServiceProvider(__DIR__."/../config/$env.json"));
 
 // definitions
-$app['url_service'] = function() {
-	return new routeManager();
-};
-echo "<pre>";
-print_r($app['databases']);
-echo "</pre>";
+#$app['url_service'] = function() {
+#	return new routeManager();
+#};
+#echo "<pre>";
+#print_r($app['databases']);
+#echo "</pre>";
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
 	'dbs.options' =>$app['databases']
 ));
 
-$sm = $app['db']->getSchemaManager();
-echo "<pre>";
+if(isset($_SERVER["HTTPS"])) {
+	$ssl = "on";
+} else { 
+	$ssl = "off";
+}
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+
+$app['route_Manager'] = $app->share(function ($app) {
+    return new routeManager($app);
+    });
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../themes/default/',
+));
+$app['twig'] = $app->share($app->extend('twig', function($twig, $app) use ($ssl) {
+    $twig->addGlobal('ssl', $ssl);
+    return $twig;
+}));
+
+$app->register(new Silex\Provider\SecurityServiceProvider(), array(
+	'security.firewalls' => $app['firewalls']
+));
+
+$app->register(new Silex\Provider\HttpCacheServiceProvider(), array(
+    'http_cache.cache_dir' => __DIR__.'/../cache/',
+));
+Request::trustProxyData();
+$routeManager = $app['route_Manager']->boot();
+
+
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__.'/../logs/'. $env . '.log',
+    'monolog.level' => Logger::WARNING
+));
+
+use Symfony\Component\Translation\Loader\YamlFileLoader;
+
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+    'locale_fallback' => 'en',
+));
+
+$app['translator'] = $app->share($app->extend('translator', function($translator, $app) {
+    $translator->addLoader('yaml', new YamlFileLoader());
+
+    $translator->addResource('yaml', __DIR__.'/../locales/en.yml', 'en');
+    $translator->addResource('yaml', __DIR__.'/../locales/de.yml', 'de');
+    $translator->addResource('yaml', __DIR__.'/../locales/fr.yml', 'fr');
+
+    return $translator;
+}));
+$app->register(new Silex\Provider\SessionServiceProvider());
+
+#$sm = $app['db']->getSchemaManager();
+#echo "<pre>";
 //var_dump($sm->createSchema());
 //$bases = $sm->listDatabases();
-$fromSchema = $sm->createSchema();
-$toSchema = clone $fromSchema;
-$toSchema->dropTable('plop');
-$sql = $fromSchema->getMigrateToSql($toSchema, $sm->getDatabasePlatform());
-print_r($sql);
-foreach ($sql as $sql_line) {
-$result = $app['db']->executeQuery($sql_line);
-}
+#$fromSchema = $sm->createSchema();
+#$toSchema = clone $fromSchema;
+#$toSchema->dropTable('plop');
+#$sql = $fromSchema->getMigrateToSql($toSchema, $sm->getDatabasePlatform());
+#print_r($sql);
+#foreach ($sql as $sql_line) {
+#$result = $app['db']->executeQuery($sql_line);
+#}
 //$plop = $app['db']->fetchAssoc('SELECT * FROM table');
 //var_dump($plop);
 /** App Definition */
-$urls = array(
-		'goog' => 'http://www.google.com',
-		'fb' => 'http://www.facebook.com',
-	     );
+#$urls = array(
+#		'goog' => 'http://www.google.com',
+#		'fb' => 'http://www.facebook.com',
+#	     );
 //$app->get('/{url_slug}',function($url_slug) use($app, $urls){
 //		return $app->redirect($app['url_service']->get($url_slug));
 //		});
-//$app->run();
+if ($app['debug']) {
+	$app->run();
+ }
+ else{
+	 $app['http_cache']->run();
+ }
 ?>
